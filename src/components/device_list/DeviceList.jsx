88318d160c7e5b38d30bacbe6963b466cd0ea322
Esca16 <znh1684@gmail.com>
2023-09-25 11:37:05 +0630

    added list name

--- src/components/device_list/DeviceList.jsx
+++ src/components/device_list/DeviceList.jsx
@@ -6,15 +6,18 @@ import { DeviceContext } from '../../context/DeviceProvider'
 const DeviceList = () => {
     const { devices } = useContext(DeviceContext)
     return (
-        <div className='flex flex-wrap gap-x-[2%] gap-y-6 mt-4'>
-            {devices.map((data) => {
-                return (
-                    <DeviceItem
-                        key={data.id}
-                        data={data}
-                    />)
-            })}
-        </div>
+        <div>
+            <div className='text-3xl font-bold ms-4'>I phone lists</div>
+            <div className='flex flex-wrap gap-x-[2%] gap-y-6 mt-4'>
+                {devices.map((data) => {
+                    return (
+                        <DeviceItem
+                            key={data.id}
+                            data={data}
+                        />)
+                })}
+            </div>
+        </div >
     )
 }
 
